3e40b9446da95cfa33d1b1915a275b88d019f60d
Leandro Pereira <leandro@leandro.io>
2023-09-15 10:51:06 -0400

    v0.1.5-dev

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule LiveMonacoEditor.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/BeaconCMS/live_monaco_editor"
-  @version "0.1.4"
+  @version "0.1.5-dev"
 
   def project do
     [
